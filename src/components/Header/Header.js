ed1bde90f2180dce89228f494e7d03dbfaefef67
s-r-aman <s.r.aman.sra@gmail.com>
2018-04-23 10:58:55 +0530

    Added Header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
+
+const Head = styled.View`
+  padding-top: 30px;
+  padding-bottom: 10px;
+  background-color: #fff;
+  elevation: 3;
+`;
+
+const Text = styled.Text`
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const SubText = styled.Text`
+  font-size: 18px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const styles = StyleSheet.create({
+  shadow: {
+    elevation: 5,
+    backgroundColor: '#FFFFFF'
+  }
+});
+
+export default () => (
+  <Head>
+    <Text>Secret Money</Text>
+    <SubText>Track your digital money!</SubText>
+  </Head>
+);
