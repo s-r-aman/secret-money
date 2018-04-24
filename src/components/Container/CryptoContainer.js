619ef7d44d9d93a63ec54c893293fccfe73ba7c6
s-r-aman <s.r.aman.sra@gmail.com>
2018-04-24 12:02:47 +0530

    Added state to container to deal with modal.

--- src/components/Container/CryptoContainer.js
+++ src/components/Container/CryptoContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components/native';
 import { ActivityIndicator } from 'react-native';
+import Modal from './../Modal/CoinInfoModal';
 
 //  Actions
 import fetchCoinData from './../../Actions/FetchCoinsData';
@@ -24,9 +25,36 @@ const ScrollView = styled.ScrollView`
 `;
 
 class CryptoContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItemSymbol: null,
+      itemContent: {},
+      modalVisibility: false
+    };
+  }
+
   componentDidMount() {
     this.props.fetchCoinData();
   }
+
+  selectItemHanlder = symbol => {
+    this.setState(() => ({
+      modalVisibility: true,
+      selectedItemSymbol: symbol,
+      itemContent: this.props.crypto
+        ? this.props.crypto.data.find(({ symbol: sym }) => sym === symbol)
+        : {}
+    }));
+  };
+
+  deselectItemHandler = () => {
+    this.setState(() => ({
+      modalVisibility: false,
+      selectedItemSymbol: null,
+      itemContent: {}
+    }));
+  };
 
   render() {
     const { crypto } = this.props;
@@ -36,8 +64,17 @@ class CryptoContainer extends Component {
           <ActivityIndicator size="large" />
         ) : (
           <ScrollView>
+            <Modal
+              {...this.state.itemContent}
+              modalVisibility={this.state.modalVisibility}
+              onRequestClose={this.deselectItemHandler}
+            />
             {crypto.data.map(coinData => (
-              <CoinCard key={coinData.symbol} {...coinData} />
+              <CoinCard
+                key={coinData.symbol}
+                {...coinData}
+                selectItemHandler={this.selectItemHanlder}
+              />
             ))}
           </ScrollView>
         )}
